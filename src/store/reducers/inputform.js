ff387cd81a9f4fe02b39235cd0d5fc76607c2542
Shailendra Kumar Singh <singh.shailendra.ind2@gmail.com>
2019-10-07 16:50:21 +0530

    Validation Function

--- src/store/reducers/inputform.js
+++ src/store/reducers/inputform.js
@@ -4,7 +4,15 @@ const initiaState = {
     formsIsValid: false,
     Formdata:Formdata
 };
-
+ // Check Validation
+const checkValidation = (value, rules) => {
+    let isValid = true;
+    if (rules && rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    return isValid;
+};
+  
 const reducer = (state=initiaState , action) =>{
     switch(action.type){
         case actionTypes.FORMS_VALID:
@@ -16,7 +24,7 @@ const reducer = (state=initiaState , action) =>{
                 };
                 copyFormId.value = action.event.target.value;
                 copyFormData[action.id] = copyFormId;
-                copyFormData[action.id].valid = action.checkValidation(
+                copyFormData[action.id].valid = checkValidation(
                 copyFormId.value,
                 copyFormId.validation
                 );
