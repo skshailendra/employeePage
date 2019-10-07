879f6eb905bc767b95ed60f3a1e1db53ddc9b6ba
Shailendra Kumar Singh <singh.shailendra.ind2@gmail.com>
2019-10-07 15:15:27 +0530

    Update the Modal Background

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -3,9 +3,12 @@ import React from "react";
 import classes from "./Modal.module.scss";
 
 const modal = props => (
-  <div className={props.show ? classes.Modal : classes.hideModal}>
-    {props.children}
-  </div>
+  <>
+    <div className={props.show ? classes.Modal : classes.hideModal}>
+      {props.children}
+    </div>
+    <div className={props.show ? classes.ModalBackground : classes.hideModal} />
+  </>
 );
 
 export default modal;
