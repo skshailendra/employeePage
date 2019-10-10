bfaaa22698c4ee7598f629ceb445a20c0a4fc281
Mr-Shailendra-Singh <singh.shailendra.ind2@gmail.com>
2019-10-11 00:37:44 +0530

    Adding Test Files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import EmployeeApp from './components/EmployeeApp/EmployeeApp';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Connect enzyme to react
+configure({adapter: new Adapter()});
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe('<App>',()=>{
+  it('should render <EmployeeApp> element when server started',()=>{
+    const wrapper = shallow(<EmployeeApp/>);
+    expect(wrapper.exists);
+  });
+  it('should render <EmployeeApp> element when server started',()=>{
+    const wrapper = shallow(<EmployeeApp/>);
+    expect(wrapper);
+  });
 });
