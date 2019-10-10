d076439d45256df2d544e0dc10ee187b64fb85dc
Shailendra Kumar Singh <singh.shailendra.ind2@gmail.com>
2019-10-10 15:58:14 +0530

    actions created

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,2 +1,14 @@
 export const GET_STARTED = 'GET_STARTED';
 export const FORMS_VALID = 'FORMS_VALID';
+
+export const getStarted = () => {
+  return {
+    type: GET_STARTED
+  };
+};
+
+export const formValid = () => {
+  return {
+    type: FORMS_VALID
+  };
+};
